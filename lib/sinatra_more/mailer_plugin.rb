625dd3cb4e47aad4edd8f16c2b50b47a5d6ace30
humancopy <info@humancopy.net>
2012-11-18 17:26:36 +0100

    Load Pony only after registering mail plugin

--- lib/sinatra_more/mailer_plugin.rb
+++ lib/sinatra_more/mailer_plugin.rb
@@ -1,11 +1,11 @@
 require 'tilt'
 require File.dirname(__FILE__) + '/support_lite'
-require File.dirname(__FILE__) + '/../../vendor/pony/lib/pony'
 Dir[File.dirname(__FILE__) + '/mailer_plugin/**/*.rb'].each { |file| load file }
 
 module SinatraMore
   module MailerPlugin
     def self.registered(app)
+      require File.dirname(__FILE__) + '/../../vendor/pony/lib/pony'
       MailerBase::views_path = app.views
     end
   end  
